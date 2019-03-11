7ff0885703518f8fa6791cafd08a9f8465c85ec4
Your Name <you@example.com>
2019-03-12 10:35:54 +1300

    commit

---
+++ Interface 2/Interface.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Interface_2
+{
+    interface Interface1
+    {
+        void Test();
+        void Show();
+    }
+    interface Interface2
+    {
+        void Test();
+        void Show();
+    }
+    class MultipleInheritanceClass : Interface1, Interface2
+    {
+        public void Show() // Implement interface
+        {
+            Console.WriteLine("Declared in interface2 and implemented in child class.");
+        }
+
+        public void Test()
+        {
+            Console.WriteLine("Interface method implemented in child class.");
+        }
+
+        void Interface1.Show() // Implement interface explicitly
+        {
+            Console.WriteLine("Declared in interface1 and implemented in child class.");
+        }
+    }
+}
---
+++ Interface 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Interface_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultipleInheritanceClass obj = new MultipleInheritanceClass();
+            obj.Test();
+            obj.Show();
+
+            Interface1 i1 = obj;
+            i1.Show();
+
+            Interface2 i2 = obj;
+            i2.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
+// A class can have one and only one immediate parent class,
+// whereas the same class can have any number of interfaces as it's parent 
+// i.e. multiple inheritance is supported thru interfaces
